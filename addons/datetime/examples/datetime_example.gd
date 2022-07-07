8b1ce7e16ea4948c9559bdcd3d411ccd6afe18b2
verillious <verilliousdev@gmail.com>
2022-07-07 19:26:53 +0100

    use unix_time

--- addons/datetime/examples/datetime_example.gd
+++ addons/datetime/examples/datetime_example.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var datetime = DateTime.datetime()
+var datetime = DateTime.now()
 
 var _delta_index := 0.0
 var _speed := 1.0
@@ -8,9 +8,8 @@ var _speed := 1.0
 
 func _process(delta: float) -> void:
 	_delta_index += delta
-	if _delta_index >= 0.06:
-		datetime.add_time(0, 0, 0, 0, 0, 1)
+	if _delta_index >= 1.0:
+		datetime.add_seconds(1)
 		_delta_index = 0.0
 
 	$CenterContainer/VBoxContainer/Label.text = datetime.get_datetime_string()
-	print()
